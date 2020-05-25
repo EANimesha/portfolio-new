07eb63023406a73852c22c1794f3e79440cce7e2
EANimesha <nimeshadilini999@gmail.com>
2020-05-25 23:14:57 +0530

    added routes and implemented the header top bar

---
+++ src/components/header/header.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import './header.css';
+import {Fade} from 'react-reveal';
+import {Link,NavLink} from 'react-router-dom';
+
+class Header extends Component {
+  render() {
+    return (
+      <Fade top duration={1000} distance="20px">
+        <div>
+          <header className="header">
+            <NavLink to="/splash" tag={Link} className="logo">
+              <span className="grey-color"> &lt;</span>
+              <span className="logo-name">Nimesha_Dilini</span>
+              <span className="grey-color">/&gt;</span>
+            </NavLink>
+            <input className="menu-btn" type="checkbox" id="menu-btn" />
+            <label className="menu-icon" htmlFor="menu-btn">
+              <span className="navicon"></span>
+            </label>
+            <ul className="menu">
+              <li>
+                <NavLink
+                  to="/home"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/education"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                >
+                  Education
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/experience"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                >
+                  Experience
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/projects"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                >
+                  Projects
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/opensource"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                >
+                  Open Source
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  tag={Link}
+                  activeStyle={{ fontWeight: "bold" }}
+                >
+                  Contact Me
+                </NavLink>
+              </li>
+            </ul>
+          </header>
+        </div>
+      </Fade>
+    );
+  }
+}
+
+export default Header;
